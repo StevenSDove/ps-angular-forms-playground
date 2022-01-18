fa8292c882b3c1f989770bb8919ca006a780bf60
StevenSDove <steven.dove@gmail.com>
2022-01-18 08:40:46 -0500

    basic form setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserSettingsFormComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
